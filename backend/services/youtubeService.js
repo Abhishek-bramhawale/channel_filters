3662d388722d3461b3096ce87aaa87780b49ecd4
Abhishek-bramhawale <sanjivkumarbramhawale4992@gmail.com>
2025-07-27 22:00:19 +0530

    retrive channel uID from channel url

---
+++ backend/services/youtubeService.js
@@ -0,0 +1,40 @@
+const { google } = require('googleapis')
+
+class YouTubeService {
+  constructor() {
+    this.youtube = google.youtube({
+      version: 'v3',
+      auth: process.env.YOUTUBE_API_KEY
+    })
+  }
+
+  
+  async getChannelIdFromUrl(url) { 
+    let channelId = null
+    if (url.includes('/channel/')) {
+      channelId = url.split('/channel/')[1].split(/[/?#]/)[0]
+    } else if (url.includes('/@')) {
+      const username = url.split('/@')[1].split(/[/?#]/)[0]
+      channelId = await this.searchChannelByUsername(username)
+    } else if (url.includes('/c/') || url.includes('/user/')) {
+      const channelName = url.split(/\/[cu]\//)[1].split(/[/?#]/)[0]
+      channelId = await this.searchChannelByUsername(channelName)
+    }
+    if (!channelId) throw new Error('Invalid YouTube channel URL')
+    return channelId
+  }
+
+  async searchChannelByUsername(query) { //actual api hitting to GET https://www.googleapis.com/youtube/v3/search
+
+    const response = await this.youtube.search.list({
+      part: 'snippet',
+      q: query,
+      type: 'channel',
+      maxResults: 1
+    })
+    if (response.data.items.length === 0) throw new Error('Channel not found')
+    return response.data.items[0].snippet.channelId
+  }
+}
+
+module.exports = new YouTubeService()
